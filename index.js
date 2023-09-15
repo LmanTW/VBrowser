9fcf35f8e24bc7d217324bfcab2bc0bf20d4d70d
LmanTW <82919705+LmanTW@users.noreply.github.com>
2023-09-15 23:02:35 +0800

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-require('./Module/Server/AccountsData').loadAccountsData()
-let httpServer = require('./Module/Server/HttpServer')()
-let socketServer = require('./Module/Server/SocketServer')(httpServer)
-
-//https://coolors.co/080708-3772ff-df2935-fdca40-e6e8e6
